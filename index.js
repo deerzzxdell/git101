917d86d9b1831bc2f529af2bfaf6141b3bab59da
Puhnyanuj <puhnyanujs@cmkl.ac.th>
2021-08-31 14:38:42 +0700

    Add change

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ ReactDOM.render(
 );
 //vvvvv
 //hhhhhhhhh
+//new change
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
